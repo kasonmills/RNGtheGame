1ad908a7a2653710e956845d7ab82fed81b7a55e
kason <kmillsventura@byui.edu>
2024-09-06 16:13:56 -0600

    initial commit

---
+++ game_files/Director.cs
@@ -0,0 +1,11 @@
+using system;
+
+namespace game;
+
+class Director
+{
+    /*
+    this class will direct the whole ordeal of the game. I want this to be the nexus of all things involved.
+    */
+    
+}
---
+++ game_files/Program.cs
@@ -0,0 +1,15 @@
+﻿using system;
+
+namespace game;
+
+class Program
+{
+    /*
+    this is the main program hub all will be controled by the director
+    */
+    static void Main(string[] args)
+    {
+        Director director = new Director();
+    }
+}
+
