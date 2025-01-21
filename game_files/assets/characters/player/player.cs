c80d152b58382e078d79035d548e363940770b57
kasonmills <kmillsventura@byui.edu>
2025-01-20 17:39:58 -0700

    some more work done.

---
+++ game_files/assets/characters/player/player.cs
@@ -0,0 +1,8 @@
+using system;
+
+namespace game;
+
+/*
+this file will be responsible for several things invovling the player. It will be the file that lets the player move throught the game
+and 
+*/
