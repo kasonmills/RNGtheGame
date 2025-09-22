ea936d427cf4baa1e2ccc521ca6d21c763855b0d
KasonMills <kmillsventura@byui.edu>
2025-09-22 14:58:47 -0600

    starting again.

--- game_files/obj/Debug/net8.0/game_files.AssemblyInfo.cs
+++ game_files/obj/Debug/net8.0/game_files.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("game_files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f562885ad62140d19ecedc17cbc98b4b3fd5f3b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b29dd7ead7ecfc76919c4c578470ea17089c0d96")]
 [assembly: System.Reflection.AssemblyProductAttribute("game_files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("game_files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
